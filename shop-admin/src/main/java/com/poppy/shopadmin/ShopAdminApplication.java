a96ed5831720763698e4d70c89907392d6726dd8
Poppy <denghuan1996@163.com>
2018-06-20 22:51:41 +0800

    初始化提交

---
+++ shop-admin/src/main/java/com/poppy/shopadmin/ShopAdminApplication.java
@@ -0,0 +1,12 @@
+package com.poppy.shopadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAdminApplication.class, args);
+	}
+}
